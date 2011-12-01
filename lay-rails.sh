1c6a69b85ba16ec79d5454683ef272ce5ac38051
kano4 <shinji.warp@gmail.com>
2011-12-01 18:29:01 +0900

    add lay-rails.sh

---
+++ lay-rails.sh
@@ -0,0 +1,60 @@
+#!/bin/sh
+app=$1
+
+if [ -z $app ]
+then
+  echo "[usage] ./lay_rails.sh (AppName)"
+  exit 0
+elif [ -e $app ]
+then
+  echo "$app is already existing"
+  exit 0
+fi
+
+echo "Start Creating ${app}"
+mkdir $app
+cd $app
+
+echo "install rails"
+
+# Gemfile
+echo "source :rubygems
+
+gem 'rails',          '~> 3.1.3'
+
+# db
+gem 'pg'
+
+# assets
+group :assets do
+  gem 'sass-rails',   '~> 3.1.5.rc.2'
+  gem 'coffee-rails', '~> 3.1.1'
+  gem 'uglifier',     '>= 1.0.3'
+end
+gem 'jquery-rails'
+
+group :development, :test do
+  gem 'rspec-rails',  '~> 2.7.0'
+  gem 'thin',         '~> 1.3.1'
+end
+" > Gemfile
+
+bundle install --binstubs --path vendor/bundle
+expect -c "
+spawn bundle exec rails new . -T -d postgresql --skip-bundle
+expect \"Ynaqdh\"; send \"n\r\"
+interact
+"
+git init
+
+# .gitignore
+echo "
+/.bundle
+/bin
+/db/*.sqlite3
+/log/*.log
+/tmp
+/vendor/bundle
+" >> .gitignore
+
+bundle exec rails g rspec:install
